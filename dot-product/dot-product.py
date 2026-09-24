964018a68e3cea9c127d0b1376d42fbe8b037b9f
Deepika Jandu <104681486+Deepika-jandu@users.noreply.github.com>
2026-09-24 23:21:37 +0530

    Solve dot-product

---
+++ dot-product/dot-product.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def dot_product(x: list, y: list) -> float:
+    """
+    Returns the dot product as a float.
+    """
+    # Write code here
+    Output = float(np.dot(x, y))
+    return Output
+    pass
